1d21aea8709a2ad565d56f2c57d21e2c29aeb299
Thomas van der Veen <thomas@owow.io>
2021-11-29 11:39:38 +0100

    Update rollup file.

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,33 @@
+import copy from 'rollup-plugin-copy';
+import json from '@rollup/plugin-json';
 import pkg from './package.json';
-import typescript from 'rollup-plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
 
-const createEntry = (options) => {
-    const config = {
-        external: [
-            'axios',
-            'collect.js',
-            'lodash.clonedeep',
-        ],
-        input: 'src/index.ts',
-        output: {
-            file: options.file,
-            format: options.format,
-            name: 'ModelCollection',
-        },
-        plugins: [
-            typescript({}),
-        ],
-    };
+const createEntry = (options) => ({
+    file: options.file,
+    format: options.format,
+    name: 'ModelCollection',
+});
 
-    return config;
+export default {
+    external: [
+        'axios',
+        'collect.js',
+        'lodash.clonedeep',
+    ],
+    input: 'src/index.ts',
+    output: [
+        createEntry({format: 'es', file: pkg.module}),
+        createEntry({format: 'cjs', file: pkg.main}),
+    ],
+    plugins: [
+        json(),
+        typescript(),
+        copy({
+            targets: [
+                {src: 'package.json', dest: 'dist'},
+                {src: 'README.md', dest: 'dist'},
+            ],
+        }),
+    ],
 };
-
-export default [
-    createEntry({format: 'es', file: pkg.module}),
-    createEntry({format: 'cjs', file: pkg.main}),
-];
