1d0213e2f08a28e5dd7c37442d91ba1088cc5836
Thomas van der Veen <thomas@owow.io>
2021-11-30 16:26:41 +0100

    fix(ts): export HttpModel

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,6 @@ export {default as Base} from './Base';
 export {default as Collection} from './Collection';
 // export {default as CollectJs} from './CollectJs';
 export {default as Errors} from './Errors';
+export * from './Model';
+export {default as HttpModel} from './Model';
 export {default as Model} from './Model';
